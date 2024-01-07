011b27c18202c7ebcc7029e8f1e0b3d4cffe1ab2
Martins Davis Bernhards <martinsdavisbernhards@gmail.com>
2024-01-07 14:40:31 +0100

    Added animtions and added bouncier physics
    Added animation for hitting redBall,
    Added speedup when hitting redBall and green bumper,
    Added bounce to rainbow ball

---
+++ Scenes/red_ball.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_area_2d_body_entered(body):
+	var groups = body.get_groups()
+	if  groups and groups[0] == "PlayerBall":
+		$Sprite.play("Hit")
+		body.linear_velocity = body.linear_velocity.normalized() * 1300
